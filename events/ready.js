b3a728ff1f3490f293c0d5dd84889f84e6ecd360
Thijs Boerma <th.boerma@gmail.com>
2025-03-28 14:51:16 +0100

    Add Status Message when online

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,19 @@
-const { Events } = require("discord.js");
+const { Events, ActivityType } = require("discord.js");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
     console.log(`WE RISE! The Hour known as ${client.user.tag} has logged in.`);
+
+    client.user.setPresence({
+      activities: [
+        {
+          name: "the Door in the Eye",
+          type: ActivityType.Watching,
+        },
+      ],
+      status: "online",
+    });
   },
 };
